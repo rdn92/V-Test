157ae002f31d346420bb3a6e70345e8ad3ccac87
Matija Tosic <tosic.matija@gmail.com>
2016-11-09 11:27:14 +0100

    working2

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,66 +1,3 @@
-<<<<<<< dcf4e3aa5b6c5c21245c8f0756a643883fbd02fc
-{{--<!DOCTYPE html>--}}
-{{--<html lang="en">--}}
-  {{--<head>--}}
-    {{--<meta charset="utf-8">--}}
-    {{--<meta http-equiv="X-UA-Compatible" content="IE=edge">--}}
-    {{--<meta name="viewport" content="width=device-width, initial-scale=1">--}}
-    {{--<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->--}}
-    {{--<title>Logo</title>--}}
-
-    {{--<!-- Bootstrap -->--}}
-    {{--<link href="css/bootstrap.min.css" rel="stylesheet">--}}
-    {{--<link href="css/style.css" rel="stylesheet">--}}
-    {{--<link href="css/signin.css" rel="stylesheet">--}}
-
-    {{--<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->--}}
-    {{--<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->--}}
-    {{--<!--[if lt IE 9]>--}}
-      {{--<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>--}}
-      {{--<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>--}}
-    {{--<![endif]-->--}}
-
-  {{--</head>--}}
-  {{--<body>--}}
-    {{--<div class="container">--}}
-      {{--@include('partials.header')--}}
-
-      {{--@yield('content')--}}
-
-    {{--</div>--}}
-
-    {{--@include('partials.footer')--}}
-
-  {{--</body>--}}
-{{--</html>--}}
-
-
-
-<html>
-<head>
-  <title>Angular QuickStart</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <style>
-
-    .done {
-      text-decoration: line-through;
-    }
-  </style>
-</head>
-<!-- 3. Display the application -->
-<body>
-<todo-app>Loading...</todo-app>
-
-<script
-        src="https://code.jquery.com/jquery-3.1.1.min.js"
-        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-</body>
-</html>
-=======
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -93,4 +30,3 @@
 
   </body>
 </html>
->>>>>>> working
